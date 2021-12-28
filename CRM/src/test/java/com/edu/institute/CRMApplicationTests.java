49d912ebe35f6b83154480df9750698e0f120f00
Alejandro Rivera <francisco.rivera@entercs.com.mx>
2021-12-28 13:52:23 -0600

    Comit inicial con Spring boot. Close #1

---
+++ CRM/src/test/java/com/edu/institute/CRMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.institute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CRMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
